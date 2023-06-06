1b9a669625e8abd4d1c58717b7482a5e2e2c17b4
kimseokjin0324 <kimseokjin0324@gmail.com>
2023-06-07 01:51:14 +0900

    Runtime Permmision 권한 , FusedLocationProvider를 사용하여 위치가져오기

--- app/src/main/java/fastcampus/aop/part4/aop_part4_chapter06/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part4/aop_part4_chapter06/MainActivity.kt
@@ -1,11 +1,89 @@
 package fastcampus.aop.part4.aop_part4_chapter06
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import fastcampus.aop.part4.aop_part4_chapter06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var cancellationTokenSource: CancellationTokenSource? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initVariables()
+
+        //- 1.앱 시작하자마자 Permission 요청
+        //- 2.Permission부여 -> 다음단계 아닐시 종료
+        requestLocationPermissions()
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (!locationPermissionGranted) {
+            finish()
+        } else {
+            //fetchData
+            cancellationTokenSource = CancellationTokenSource()
+
+
+            fusedLocationProviderClient.getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener { location ->
+                binding.textView.text = "${location.latitude}+${location.longitude}"
+
+            }
+        }
+
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
